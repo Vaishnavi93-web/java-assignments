d4a1ffd9ce06133c019a7a0aa6479d7efa688695
Vaishnavi Bande <bandevaishnavi93@gmail.com>
2025-07-08 15:54:46 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,32 @@
+public class Calculator {
+
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    double subtract(double a, double b) {
+        return a - b;
+    }
+
+    double multiply(double a, double b) {
+        return a * b;
+    }
+
+    double divide(double a, double b) {
+        if (b != 0) {
+            return a / b;
+        } else {
+            System.out.println("Division by zero is not allowed.");
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        System.out.println("Addition: " + calc.add(10, 5));
+        System.out.println("Subtraction: " + calc.subtract(10, 5));
+        System.out.println("Multiplication: " + calc.multiply(10, 5));
+        System.out.println("Division: " + calc.divide(10, 5));
+    }
+}
