1a64e56f6638f886980c4ba392d21a3fb71c3a4e
Vaishnavi Bande <bandevaishnavi93@gmail.com>
2025-07-09 20:00:10 +0530

    Create EvenOddCheck.java

---
+++ EvenOddCheck.java
@@ -0,0 +1,11 @@
+public class EvenOddCheck {
+    public static void main(String[] args) {
+        int num = 8;
+
+        if (num % 2 == 0) {
+            System.out.println("Number is even.");
+        } else {
+            System.out.println("Number is odd.");
+        }
+    }
+}
