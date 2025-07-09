e2b0ddd43ba4bd902ea1547ec824a63aae9e47ca
Vaishnavi Bande <bandevaishnavi93@gmail.com>
2025-07-09 20:08:21 +0530

    Create AdultCheck.java

---
+++ AdultCheck.java
@@ -0,0 +1,11 @@
+public class AdultCheck {
+    public static void main(String[] args) {
+        int age = 21; 
+
+        if (age >= 18) {
+            System.out.println("Person is an adult.");
+        } else {
+            System.out.println("Person is not an adult.");
+        }
+    }
+}
