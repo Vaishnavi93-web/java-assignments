f1818b5667261101598a430c56b723545500ca19
Vaishnavi Bande <bandevaishnavi93@gmail.com>
2025-07-09 20:06:10 +0530

    Create GreaterThan100.java

---
+++ GreaterThan100.java
@@ -0,0 +1,10 @@
+public class GreaterThan100 {
+    public static void main(String[] args) {
+        int num = 110; 
+        if (num > 100) {
+            System.out.println("Number is greater than 100.");
+        } else {
+            System.out.println("Number is not greater than 100.");
+        }
+    }
+}
