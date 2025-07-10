26becc740b927aa1989076509fe81c4219265778
Vaishnavi Bande <bandevaishnavi93@gmail.com>
2025-07-10 21:56:29 +0530

    Create DivisibleBy5.java

---
+++ DivisibleBy5.java
@@ -0,0 +1,11 @@
+public class DivisibleBy5 {
+    public static void main(String[] args) {
+        int num = 25; 
+
+        if (num % 5 == 0) {
+            System.out.println("Number is divisible by 5.");
+        } else {
+            System.out.println("Number is not divisible by 5.");
+        }
+    }
+}
